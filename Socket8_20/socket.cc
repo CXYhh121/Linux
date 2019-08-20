a08fb5475115811faa1ea518bb44ad0009a3a1a3
CXYhh121 <1637018959@qq.com>
2019-08-20 17:27:05 +0800

    socket编程接口的学习与应用

---
+++ Socket8_20/socket.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <arpa/inet.h>
+#include <socket.h>
+
+using namespace std;
+
+int main()
+{
+    uint32_t htonl(uint32_t hostlong);
+    uint16_t htons(uint16_t hostshort);
+    uint32_t ntohl(uint32_t netlong);
+    uint16_t ntohs(uint16_t netshort);
+
+
+
+    return 0;
+}
